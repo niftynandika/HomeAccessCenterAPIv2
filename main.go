524372c93f5f6bb97c444e050173f729230e76ed
niftynandika <71160536+niftynandika@users.noreply.github.com>
2025-07-24 13:00:53 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/niftynandika/HomeAccessCenterAPIv2/api"
+import (
+	"log"
+	"os"
+
+	"github.com/niftynandika/HomeAccessCenterAPIv2/api"
+)
 
 func main() {
-	api.RunServer()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default if not running on Render or a cloud platform
+	}
+
+	app := api.SetupRouter() // Call a function from your api package to get the router
+	log.Printf("Starting server on port %s...", port)
+	err := app.Run(":" + port)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
